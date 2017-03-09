74e537853790f3a4ade621bc73a7ab80cdbbfe4a
Benchan23 <bednayb@gmail.com>
2017-03-09 10:37:17 +0100

    correct comment about the model create or modified

--- src/shortener/models.py
+++ src/shortener/models.py
@@ -6,9 +6,9 @@ class BenURL(models.Model):
     url = models.CharField(max_length=200, )
     # every shortcode should be unique
     shortcode = models.CharField(max_length=15, unique=True)
-    # when was the model created
-    created_datetime = models.DateTimeField(auto_now = True)
     #  when was the model modified last time
+    created_datetime = models.DateTimeField(auto_now = True)
+    # when was the model created
     update_timestamp = models.DateTimeField(auto_now_add = True)
 
     def __str__(self):
