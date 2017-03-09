ef6993d3573a205423425057945061d4da68865a
Benchan23 <bednayb@gmail.com>
2017-03-09 10:31:21 +0100

    add create and update time

--- src/shortener/models.py
+++ src/shortener/models.py
@@ -1,9 +1,15 @@
 from django.db import models
+import datetime
 
 # Create your models here.
 class BenURL(models.Model):
     url = models.CharField(max_length=200, )
+    # every shortcode should be unique
     shortcode = models.CharField(max_length=15, unique=True)
+    # when was the model created
+    created_datetime = models.DateTimeField(auto_now = True)
+    #  when was the model modified last time
+    update_timestamp = models.DateTimeField(auto_now_add = True)
 
     def __str__(self):
         return str(self.url)
