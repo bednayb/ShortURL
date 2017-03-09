445a82827bbffbd4da5c19306e4fcc2a39c58561
Benchan23 <bednayb@gmail.com>
2017-03-09 14:04:20 +0100

    add shortcode ganerator with process

--- src/shortener/models.py
+++ src/shortener/models.py
@@ -1,15 +1,24 @@
 from django.db import models
 import datetime
-
+from .utils import shortcode_generator, create_shortcode
 # Create your models here.
+
+# create a shortcode generator (any string or digit, 8 character)
+
 class BenURL(models.Model):
     url = models.CharField(max_length=200, )
     # every shortcode should be unique
-    shortcode = models.CharField(max_length=15, unique=True)
+    shortcode = models.CharField(max_length=15, unique=True, blank= True)
     #  when was the model modified last time
     created_datetime = models.DateTimeField(auto_now = True)
     # when was the model created
     update_timestamp = models.DateTimeField(auto_now_add = True)
+
+    def save(self, *args, **keyword_args):
+        # create shortcode if the url doesnt have it
+        if self.shortcode is None or self.shortcode == "":
+            self.shortcode = create_shortcode(self)
+        super(BenURL, self).save(*args, **keyword_args)
 
     def __str__(self):
         return str(self.url)
---
+++ src/shortener/utils.py
@@ -0,0 +1,14 @@
+import random
+import string
+
+
+def shortcode_generator(size = 8, chars = string.ascii_lowercase + string.digits):
+    return ''.join(random.choice(chars) for i in range(size))
+
+def create_shortcode(instance, size = 8):
+    new_code = shortcode_generator(size = size)
+    Klass = instance.__class__
+    qs_exists = Klass.objects.filter(shortcode= new_code).exists()
+    if qs_exists:
+        return create_shortcode(size=size)
+    return new_code
